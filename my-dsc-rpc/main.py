f932e7d6b7f1b061101b0f73fb81abd5c47b069d
MannuVilasara <mannuvilasara@gmail.com>
2024-05-01 22:49:49 +0530

    remove time elapsed

--- my-dsc-rpc/main.py
+++ my-dsc-rpc/main.py
@@ -1,5 +1,5 @@
 from pypresence import Presence
-from time import sleep, time
+from time import sleep
 from utils import get_memory_usage, get_cpu_usage, songinfo, get_system_uptime
 from colorama import Fore, Style
 
@@ -8,9 +8,9 @@ presence.connect()
 
 while True:
     info = songinfo()
-    details = f"▶️ {info['artist']} - {info['title']}"
+    details = f"▶️ {info['title']}"
     if info["status"] == "Paused":
-        details = f"⏸️ {info['artist']} - {info['title']}"
+        details = f"⏸️ {info['title']}"
     large_text = f"Playing {info['artist']} - {info['title']} "
     if info["status"] == "Paused":
         large_text = f"Paused at {info['artist']} - {info['title']}"
@@ -21,7 +21,6 @@ while True:
         large_text=large_text,
         small_image="https://avatars.githubusercontent.com/u/107882187?s=200&v=4",
         small_text=f"Hyprland | the current song is {info['title']} by {info['artist']}. My laptop has been up for {str(get_system_uptime())[0]}hr and {str(get_system_uptime())[2:4]}min",
-        start=int(time()),
         buttons=[{"label": "Install Arch", "url": "https://archlinux.org"}],
     )
     print(
