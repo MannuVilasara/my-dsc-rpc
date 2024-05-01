84e95e7b5ce19c4cdfc1477dd1b1fc7da44a0fdf
MannuVilasara <mannuvilasara@gmail.com>
2024-05-01 16:49:11 +0530

    implemeted rpc

--- my-fetch/main.py
+++ my-fetch/main.py
@@ -1,51 +1,31 @@
 from pypresence import Presence
-import subprocess
-import sys
-import psutil
+from time import sleep, time
+from utils import get_memory_usage,get_cpu_usage, songinfo, get_system_uptime
+from colorama import Fore, Style
 
-def run_command(command):
-    try:
-        # Run the command
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        output, error = process.communicate()
+presence = Presence("1235105826016858143")
+presence.connect()
 
-        # Decode output
-        output = output.decode('utf-8').strip()
-        error = error.decode('utf-8').strip()
+while True:
+    info = songinfo()
+    details = f"▶️ {info['artist']} - {info['title']}"
+    if info['status'] == "Paused":
+        details = f"⏸️ {info['artist']} - {info['title']}"
+    large_text = f"Playing {info['artist']} - {info['title']} "
+    if info['status'] == "Paused":
+        large_text= f"Paused at {info['artist']} - {info['title']}"
+    presence.update(
+        state=f"💻 CPU {get_cpu_usage()}%, RAM {get_memory_usage()['used_memory']}/{get_memory_usage()['total_memory']} GB | {get_memory_usage()["memory_percent"]}% used",
+        details=details,
 
-        # Check for errors
-        if process.returncode != 0:
-            print(f"Error executing command '{command}': {error}", file=sys.stderr)
-            return None
-        else:
-            return output
-    except Exception as e:
-        print(f"An error occurred: {str(e)}", file=sys.stderr)
-        return None
-    
-def get_cpu_usage():
-    cpu_usage = psutil.cpu_percent(interval=1)
-    return cpu_usage
+        large_image=info['artUrl'],
+        large_text=large_text,
 
-def bytes_to_gigabytes(bytes):
-    return bytes / (1024 * 1024 * 1024)
+        small_image="https://avatars.githubusercontent.com/u/107882187?s=200&v=4",
+        small_text=f"Hyprland | the current song is {info['title']} by {info['artist']}. My laptop has been up for {str(get_system_uptime())[0]}hr and {str(get_system_uptime())[2:4]}min",
 
-def get_memory_usage():
-    # Get memory usage statistics
-    mem = psutil.virtual_memory()
-
-    # Total physical memory (RAM) in gigabytes
-    total_memory = bytes_to_gigabytes(mem.total)
-    # Available physical memory (RAM) in gigabytes
-    available_memory = bytes_to_gigabytes(mem.available)
-    # Used physical memory (RAM) in gigabytes
-    used_memory = bytes_to_gigabytes(mem.used)
-    # Percentage of used memory
-    memory_percent = mem.percent
-
-    return {
-        'total_memory': total_memory,
-        'available_memory': available_memory,
-        'used_memory': used_memory,
-        'memory_percent': memory_percent
-    }
+        start=int(time()),
+        buttons=[{"label": "Install Arch", "url": "https://archlinux.org"}],
+    )
+    print(f"=> {Fore.MAGENTA}INFO:{Style.RESET_ALL} {Fore.CYAN}[ARCH-LINUX]{Style.RESET_ALL} (archbtw)| Uptime: {get_system_uptime()}, {Fore.RED}CPU: {get_cpu_usage()}%{Style.RESET_ALL}, {get_memory_usage()['memory_percent']}% mem used.")
+    sleep(15)
---
+++ my-fetch/utils.py
@@ -0,0 +1,65 @@
+import subprocess
+import sys
+import psutil
+import datetime
+
+def get_system_uptime():
+    # Get system uptime in seconds
+    uptime_seconds = psutil.boot_time()
+    
+    # Convert uptime to a timedelta object
+    uptime_timedelta = datetime.datetime.now() - datetime.datetime.fromtimestamp(uptime_seconds)
+    
+    return uptime_timedelta
+
+def run_command(command):
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+
+        output = output.decode('utf-8').strip()
+        error = error.decode('utf-8').strip()
+
+        if process.returncode != 0:
+            print(f"Error executing command '{command}': {error}", file=sys.stderr)
+            return None
+        else:
+            return output
+    except Exception as e:
+        print(f"An error occurred: {str(e)}", file=sys.stderr)
+        return None
+    
+def get_cpu_usage():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    return cpu_usage
+
+def bytes_to_gigabytes(bytes):
+    return round(bytes / (1024 * 1024 * 1024), 1)
+
+def get_memory_usage():
+    mem = psutil.virtual_memory()
+    total_memory = bytes_to_gigabytes(mem.total)
+    used_memory = bytes_to_gigabytes(mem.used)
+    memory_percent = mem.percent
+
+    return {
+        'total_memory': total_memory,
+        'used_memory': used_memory,
+        'memory_percent': memory_percent
+    }
+
+def songinfo():
+    status = "Playing"
+    _is_playing = run_command("playerctl status")
+    if _is_playing == "Stopped":
+        return None
+    if _is_playing == "Paused":
+        status = "Paused"
+    return {
+        'status': status,
+        'artist': run_command("playerctl metadata artist"),
+        'title': run_command("playerctl metadata title"),
+        'artUrl': run_command("playerctl metadata mpris:artUrl"),
+        'track': run_command("playerctl metadata xesam:url"),
+        # 'duration': run_command("playerctl metadata --format '{{ duration(position) }}/{{ duration(mpris:length) }}'")
+    }
