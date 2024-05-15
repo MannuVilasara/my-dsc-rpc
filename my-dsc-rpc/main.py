b02fbb520ff1329a5ac52c39bd2c51d489e9581e
MannuVilasara <mannuvilasara@gmail.com>
2024-05-15 12:31:13 +0530

    fix: optimized

--- my-dsc-rpc/main.py
+++ my-dsc-rpc/main.py
@@ -7,7 +7,9 @@ presence = Presence("1235105826016858143")
 presence.connect()
 
 while True:
+    mem_usage = get_memory_usage()
     uptime = str(get_system_uptime())
+    cpu = get_cpu_usage()
     info = songinfo()
     big_img = info["artUrl"]
     if info["status"] == "Stopped":
@@ -24,15 +26,15 @@ while True:
         minutes = uptime[3:5]
         hours = uptime[0:2]
     presence.update(
-        state=f"💻 CPU {get_cpu_usage()}%, RAM {get_memory_usage()['used_memory']}/{get_memory_usage()['total_memory']} GB | {get_memory_usage()["memory_percent"]}% used",
+        state=f"💻 CPU {cpu}%, RAM {mem_usage['used_memory']}/{mem_usage['total_memory']} GB | {mem_usage["memory_percent"]}% used",
         details=details,
         large_image=big_img,
         large_text=large_text,
         small_image="https://avatars.githubusercontent.com/u/107882187?s=200&v=4",
         small_text=f"Hyprland | the current song is {info['title']} by {info['artist']}. My laptop has been up for {hours}hr and {minutes}min",
-        buttons=[{"label": "Install Arch", "url": "https://archlinux.org"}],
+        buttons=[{"label": "Install Arch", "url": "https://archlinux.org"},{"label": "Github", "url": "https://github.com/MannuVilasara"}],
     )
     print(
-        f"=> {Fore.MAGENTA}INFO:{Style.RESET_ALL} {Fore.CYAN}[ARCH-LINUX]{Style.RESET_ALL} (archbtw)| Uptime: {get_system_uptime()}, {Fore.RED}CPU: {get_cpu_usage()}%{Style.RESET_ALL}, {get_memory_usage()['memory_percent']}% mem used."
+        f"=> {Fore.MAGENTA}INFO:{Style.RESET_ALL} {Fore.CYAN}[ARCH-LINUX]{Style.RESET_ALL} (archbtw)| Uptime: {uptime}, {Fore.RED}CPU: {cpu}%{Style.RESET_ALL}, {mem_usage['memory_percent']}% mem used."
     )
     sleep(15)
